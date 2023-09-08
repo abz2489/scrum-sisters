9f496fd7ca6dbf7945abd56756c31e5f8cd614d3
Abigail Davies <abz2489@gmail.com>
2023-09-08 15:30:39 +0000

    Create database relationships

--- scrumsisters/models.py
+++ scrumsisters/models.py
@@ -11,6 +11,10 @@ class Users(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     confirm_password = db.Column(db.String(50), nullable=False)
+    club_id = db.Column(db.Integer, db.ForeignKey("clubs.id"), nullable=False)
+
+    # relationships
+    teams = db.relationship("Teams", backref="users", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -24,6 +28,15 @@ class Clubs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     club_name = db.Column(db.String(150), nullable=False)
 
+    # relationships
+    users = db.relationship("Users", backref="clubs", lazy=True)
+    teams = db.relationship(
+        "Teams",
+        backref="clubs",
+        cascade="all, delete",
+        lazy=True
+        )
+
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self
@@ -35,6 +48,9 @@ class Age(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     age_group = db.Column(db.String, nullable=False)
+
+    # relationships
+    teams = db.relationship("Teams", backref="age", uselist=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -48,6 +64,9 @@ class Training(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     training_day = db.Column(db.String, nullable=False)
 
+    # relationships
+    teams = db.relationship("Teams", backref="training", lazy=True)
+
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self
@@ -59,11 +78,17 @@ class Teams(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(150), nullable=False)
+    club_id = db.Column(db.Integer, db.ForeignKey("clubs.id"), nullable=False)
+    age_group_id = db.Column(
+        db.Integer, db.ForeignKey("age.id"), nullable=False)
+    training_id = db.Column(db.Integer, db.ForeignKey(
+        "training.id"), nullable=False)
     training_time = db.Column(db.Time, nullable=False)
     training_location = db.Column(db.Text, nullable=False)
     fb_url = db.Column(db.Text, nullable=True)
     tiktok_url = db.Column(db.Text, nullable=True)
     insta_url = db.Column(db.Text, nullable=True)
+    users_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
