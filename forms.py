1c954de7a8b90b68a4d23d9a586c24731695cf4d
Abigail Davies <abz2489@gmail.com>
2023-09-09 14:27:55 +0000

    Import wtforms, fields and validators

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    EmailField,
+    SelectField,
+    SelectMultipleField,
+    SubmitField
+    )
+from wtforms.validators import DataRequired
+
