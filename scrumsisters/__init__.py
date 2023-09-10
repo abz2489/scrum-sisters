c6edd06f0c4fd9b79a0a69236a5637f5ca15525f
Abigail Davies <abz2489@gmail.com>
2023-09-10 17:01:57 +0000

    Install and import Flask Migrate

--- scrumsisters/__init__.py
+++ scrumsisters/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env
 
@@ -12,5 +13,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from scrumsisters import routes  # noqa
