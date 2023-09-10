b4f95a8dc5bf278d1ca6c12274ea49d0e4cc4b91
Abigail Davies <abz2489@gmail.com>
2023-09-10 08:12:51 +0000

    Create user registration form route

--- scrumsisters/routes.py
+++ scrumsisters/routes.py
@@ -1,7 +1,30 @@
 from flask import render_template
+from forms import UserRegistrationForm
 from scrumsisters import app, db
+from scrumsisters.models import Users, Clubs, Age, Days, Teams
 
 
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    first_name = None
+    last_name = None
+    email = None
+    form = UserRegistrationForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        form.first_name.data = ''
+        last_name = form.last_name.data
+        form.last_name.data = ''
+        email = form.email.data
+        form.email.data = ''
+    return render_template(
+        "register.html",
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        form=form)
