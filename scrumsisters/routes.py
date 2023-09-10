b290639aef4a74ffe6d80b37a2748e0ead8c99fc
Abigail Davies <abz2489@gmail.com>
2023-09-10 12:04:25 +0000

    Add flashed message to user registration form

--- scrumsisters/routes.py
+++ scrumsisters/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash
 from forms import UserRegistrationForm
 from scrumsisters import app, db
 from scrumsisters.models import Users, Clubs, Age, Days, Teams
@@ -22,6 +22,7 @@ def register():
         form.last_name.data = ''
         email = form.email.data
         form.email.data = ''
+        flash("Registration successful")
     return render_template(
         "register.html",
         first_name=first_name,
