de48f20bfbd08be2388bc434f689426965339689
Abigail Davies <abz2489@gmail.com>
2023-09-16 14:09:09 +0000

    Remove clubs from wtform

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ from wtforms import (
     )
 from wtforms.validators import DataRequired, EqualTo
 from wtforms_sqlalchemy.fields import QuerySelectField
-from scrumsisters.models import clubs_list
+# from scrumsisters.models import clubs_list
 
 
 # Create a Form Class
@@ -17,9 +17,9 @@ class UserRegistrationForm(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
-    #club = QuerySelectField(query_factory=clubs_list, allow_blank=True)
-    #password = PasswordField('Password', validators=[DataRequired(), EqualTo(
-            #'confirm_password', message="Passwords do not match!")])
-    #confirm_password = PasswordField(
-        #'confirm_password', validators=[DataRequired()])
+    user_password = PasswordField(
+        'Password', validators=[DataRequired(), EqualTo(
+            'confirm_password', message="Passwords do not match!")])
+    confirm_password = PasswordField(
+        'Confirm_password', validators=[DataRequired()])
     submit = SubmitField("Submit")
