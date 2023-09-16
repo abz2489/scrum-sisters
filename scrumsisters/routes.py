20d8785c905de1e737828243b5faf7c90f692f03
Abigail Davies <abz2489@gmail.com>
2023-09-16 14:12:38 +0000

    Finish registration funcitonality

--- scrumsisters/routes.py
+++ scrumsisters/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, flash
 from forms import UserRegistrationForm
 from sqlalchemy import text
 from scrumsisters import app, db, migrate
-from scrumsisters.models import Users, Clubs, Age, Days, Teams, clubs_list
-from werkzeug.security import generate_password_hash, check_password_hash
+from scrumsisters.models import Users, Clubs, Age, Days, Teams
+from scrumsisters.models import generate_password_hash, check_password_hash
 
 
 @app.route("/")
@@ -16,19 +16,30 @@ def register():
     first_name = None
     last_name = None
     email = None
+    user_password = None
     form = UserRegistrationForm()
     # form validation
     if form.validate_on_submit():
+        # Check if user email exists already
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            # Hash password
+            password_hash = generate_password_hash(
+                form.user_password.data, method="scrypt")
+            user = Users(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                email=form.email.data,
+                user_password=password_hash
+            )
+            db.session.add(user)
+            db.session.commit()
         first_name = form.first_name.data
-        form.first_name.data = ''
-        last_name = form.last_name.data
-        form.last_name.data = ''
-        email = form.email.data
-        form.email.data = ''
     return render_template(
         "register.html",
         first_name=first_name,
         last_name=last_name,
         email=email,
+        password_hash=user_password,
         form=form
         )
