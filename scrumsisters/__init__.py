26b89286d43f7db3c3ec451e33ae8db543135b4e
Abigail Davies <abz2489@gmail.com>
2023-09-16 20:03:37 +0000

    Import flask login

--- scrumsisters/__init__.py
+++ scrumsisters/__init__.py
@@ -2,6 +2,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    login_required,
+    logout_user,
+    current_user
+)
 if os.path.exists("env.py"):
     import env
 
