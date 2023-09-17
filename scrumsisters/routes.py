939f1e33d4f6dffc0bf0eca8c384267dbb57f874
Abigail Davies <abz2489@gmail.com>
2023-09-17 10:46:40 +0000

    Add sign in functionality to routes

--- scrumsisters/routes.py
+++ scrumsisters/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 from forms import UserRegistrationForm, UserSignInForm
 from sqlalchemy import text
 from scrumsisters import app, db, migrate
 from scrumsisters.models import Users, Clubs, Age, Days, Teams
 from scrumsisters.models import generate_password_hash, check_password_hash
+from scrumsisters import login_manager, login_user
 
 
 @app.route("/")
@@ -45,7 +46,29 @@ def register():
         )
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
 @app.route("/signin", methods=["GET", "POST"])
 def user_signin():
     form = UserSignInForm()
+    # Validate log in details
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user:
+            # Check password hash
+            if check_password_hash(user.user_password, form.password.data):
+                login_user(user)
+                return redirect(url_for('user_profile'))
+            else:
+                flash("Wrong email or password")
+        else:
+            flash("User doesn't exist, try again or register for an account")
     return render_template("signin.html", form=form)
+
+
+@app.route("/profile")
+def user_profile():
+    return render_template('profile.html')
