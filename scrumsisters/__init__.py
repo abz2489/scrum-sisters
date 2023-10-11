cf440338143a789caf106850e5a509bcc3a145aa
Abigail Davies <abz2489@gmail.com>
2023-10-11 16:29:02 +0000

    Add if else statement for Heroku deployment

--- scrumsisters/__init__.py
+++ scrumsisters/__init__.py
@@ -17,8 +17,13 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 # App configuration variables
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
