b9c2c50d92cda19f9647b97fe3fbf269102ee4cb
Abigail Davies <abz2489@gmail.com>
2023-10-11 16:37:01 +0000

    Amend postgresql typo

--- scrumsisters/__init__.py
+++ scrumsisters/__init__.py
@@ -21,8 +21,8 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
+    if uri.startswith("postgresql://"):
+        uri = uri.replace("postgresql://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
