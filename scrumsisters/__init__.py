64e34927d4289dd8fdfc6aee59e447b4a60a921c
Abigail Davies <abz2489@gmail.com>
2024-03-22 00:41:52 +0000

    Update database_url to db_url

--- scrumsisters/__init__.py
+++ scrumsisters/__init__.py
@@ -22,7 +22,7 @@ app.config['SQLALCHEMY_MAX_OVERFLOW'] = 30
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DB_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
